6eff88e8b64b76599bcbf505122e2e182cffadd9
Matthew Lambrianidis <146152555+matt-lamb@users.noreply.github.com>
2026-08-23 17:27:48 +1000

    Cleaning script

--- Distort Case Estimates.R
+++ Distort Case Estimates.R
@@ -20,32 +20,9 @@ distort_case_estimates <- function(fp_in, fp_out, prop_low, prop_high, seed) {
   revisions_by_claim = data.table(claim_no = claim_numbers, revision_factor = revision_factors)
   data = merge(data, revisions_by_claim, by = "claim_no")
   
-  # 'simple' version
+  # Scale case estimates down by proportion of time remaining and round them to nearest dollar
   data[, OCL := round(OCL * (1 - revision_factor * (duration - txn_delay) / duration))]
   data[, incurred := OCL + cumpaid]
-
-  # # Where there is an existing case estimate revision,
-  # # scale case estimates down by proportion of time remaining
-  # # and round them to nearest dollar
-  # data[, ':=' (orig_incurred = incurred,
-  #              orig_OCL = OCL)]
-  # 
-  # data[txn_type != "P", OCL := round(OCL * (1 - revision_factor * (duration - txn_delay) / duration))]
-  # data[, incurred := OCL + cumpaid]
-  # 
-  # # The final revision will need to be reverted to the true incurred so that case estimates continue to converge to 0
-  # data[, last_revision := FALSE]
-  # data[txn_type != "P", last_revision := seq_len(.N) == .N, by = claim_no]
-  # 
-  # data[last_revision == TRUE, incurred := orig_incurred]
-  # data[last_revision == TRUE, OCL := incurred - cumpaid]
-  # 
-  # # Where there is only a payment,
-  # # copy the incurred from the previous transaction and update OCL accordingly
-  # data[txn_type == "P", incurred := NA]
-  # data[, incurred := nafill(incurred, type = "locf")]
-  # data[txn_type == "P", OCL := incurred - cumpaid]
-
 
   data[, ':=' (duration = NULL,
               revision_factor = NULL)]
@@ -71,4 +48,5 @@ for (i in 0:max_iter) {
   distort_case_estimates(fp_in, fp_out, prop_low, prop_high, seed_base + i)
 }
 print("Done!")
+
 
